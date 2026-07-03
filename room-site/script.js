9084cae830d05b6847973fd616c9027bb4984763
jjeremyjacob <THEJEREMYJACOB@GMAIL.COM>
2026-07-03 09:21:20 -0400

    Delete room-site directory

--- room-site/script.js
+++
@@ -1,9 +0,0 @@
-function goToRoom(roomId) {
-  const rooms = document.querySelectorAll('.room');
-
-  rooms.forEach(room => {
-    room.classList.add('hidden');
-  });
-
-  document.getElementById(roomId).classList.remove('hidden');
-}
